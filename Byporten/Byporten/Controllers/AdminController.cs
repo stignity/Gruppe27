8c25afc26afbaef15eeaffaab8a31bcb7bbc4ff2
Anders Isaksen <isaand12@student.westerdals.no>
2015-03-26 11:39:40 +0100

    Fixed initial Layout of project

--- Byporten/Byporten/Controllers/AdminController.cs
+++ Byporten/Byporten/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Byporten.Controllers
 {
@@ -11,7 +12,55 @@ namespace Byporten.Controllers
         // GET: Admin
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            return RedirectToAction("Portal", "Admin");
+        }
+
+        [HttpGet]
+        public ActionResult Portal()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Portal(Byporten.Models.AdminModel admin) {
+            if (ModelState.IsValid)
+            {
+                if (IsValid(admin.Username, admin.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(admin.Username, false);
+                    return RedirectToAction("Index", "Admin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Brukernavn eller Passord er feil. Vennligst prøv igjen.");
+                }
+            }
+
+            return View(admin);
+        }
+
+        private bool IsValid(string username, string password)
+        {
+            var crypto = new SimpleCrypto.PBKDF2();
+            bool isValid = false;
+
+            using (var db = new byportenEntities())
+            {
+                var user = db.adminuser.FirstOrDefault(u => u.Username == username);
+
+                if (user != null)
+                {
+                    if (user.Password == crypto.Compute(password, user.PasswordSalt))
+                    {
+                        isValid = true;
+                    }
+                }
+            }
+
+            return isValid;
         }
     }
 }
---
+++ Byporten/Byporten/Models/UserModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Byporten.Models
+{
+    public class UserLoginModel
+    {
+        [Required]
+        [Display(Name="Epost")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Passord")]
+        [DataType(DataType.Password)]
+        [MinLength(6), MaxLength(25)]
+        public string Password { get; set; }
+
+    }
+    public class AdminModel
+    {
+        [Required]
+        [Display(Name = "Brukernavn")]
+        [StringLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Passord")]
+        [MinLength(6), MaxLength(25)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class UserCreateModel
+    {
+
+    }
+}
