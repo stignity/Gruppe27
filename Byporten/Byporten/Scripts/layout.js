fb5e57cfd6afe6bc9f7616c4e09cdc9f342a9779
Oyvind <heroyv12@student.westerdals.no>
2015-03-26 14:08:53 +0100

    Made menu work on layout

--- Byporten/Byporten/Scripts/layout.js
+++ Byporten/Byporten/Scripts/layout.js
@@ -2,10 +2,42 @@
 *  =========================================
 *   JavaScript document for _LayoutClient
 *  =========================================
-*    Authors: Gruppe 27, Westerdals OSLO ACT
+*   Authors: Gruppe 27, Westerdals OSLO ACT
 *  =========================================
 */
 
 (function () {
 
+    var Toggled = false;
+    var toggle_main_menu = function () {
+        $('.menu-button').click(function () {
+            if (Toggled == false) {
+                Toggled = true;
+                $('.hidden-menu-wrap').animate({ left: '0px' }, 250);
+                $('body').animate({ left: '240px' }, 250);
+                $('body').css('max-width', '100%');
+                $('body').css('overflow-x', 'hidden');
+
+            } else {
+                Toggled = false;
+                $('.hidden-menu-wrap').animate({ left: '-240px' }, 250);
+                $('body').animate({ left: '0px' }, 250);
+            }
+        });
+    }
+
+    var toggle_searchbar = function () {
+
+    }
+
+
+
+    //pageload function
+    var init = function () {
+        toggle_main_menu();
+        toggle_searchbar();
+    }    
+
+    
+    init(); // calling init on page-load
 })();
