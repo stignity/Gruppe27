fb80a19937c7a4efaf1aad7132337cf23f026c28
Oyvind <heroyv12@student.westerdals.no>
2015-04-09 13:40:07 +0200

    design register view for kundeklubb is done

--- Byporten/Byporten/Scripts/layout.js
+++ Byporten/Byporten/Scripts/layout.js
@@ -44,12 +44,26 @@
         })
     }
 
+    var carousel = function () {
+        var $els = $('div[id^=quote]'),
+        i = 0,
+        len = $els.length;
+
+        $els.slice(1).hide();
+        setInterval(function () {
+            $els.eq(i).fadeOut(function () {
+                i = (i + 1) % len
+                $els.eq(i).fadeIn();
+            })
+        }, 7000)
+    }
 
 
     //pageload function
     var init = function () {
         toggle_main_menu();
         toggle_searchbar();
+        carousel();
     }    
 
     
