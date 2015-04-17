690d404d192d958ae795ae0ae9ccead5cfc8262c
Oyvind <heroyv12@student.westerdals.no>
2015-04-17 14:31:55 +0200

    added search to stores

--- Byporten/Byporten/obj/Debug/Package/PackageTmp/Scripts/layout.js
+++ Byporten/Byporten/obj/Debug/Package/PackageTmp/Scripts/layout.js
@@ -11,13 +11,13 @@
     var toggle_main_menu = function () {
         $('.menu-trigger').click(function () {
             $('.hidden-menu-wrap').animate({ right: '0px' }, 250);
-            $('.master').animate({ right: '250px' }, 250);
+            $('.master').animate({ right: '200px' }, 250);
             $('.master').css('max-width', '100%');
             $('.master').css('overflow-y', 'hidden');
             $('.menu-trigger').fadeOut();
         });
         $('.menu-header').click(function () {
-            $('.hidden-menu-wrap').animate({ right: '-250px' }, 250);
+            $('.hidden-menu-wrap').animate({ right: '-290px' }, 250);
             $('.master').animate({ right: '0px' }, 250);
             $('.menu-trigger').fadeIn();
         });   
